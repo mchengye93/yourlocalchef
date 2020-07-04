425229b9b17736a9227e0b6ee83be8f74b8d0f8b
Marbo Cheng Ye <marbochengye@gmail.com>
2020-07-03 22:55:49 -0700

    Added paginations to ListView

--- yourlocalchef/foods/api_views.py
+++ yourlocalchef/foods/api_views.py
@@ -1,8 +1,15 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from foods.serializers import UserSerializer,FoodItemSerializer
 from foods.models import User,FoodItem,SaleHistory
+
+class DefaultPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit=100
+
+
 
 class UserList(ListAPIView):
     queryset=User.objects.all()
@@ -10,6 +17,7 @@ class UserList(ListAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields=('name','location',)
     search_fields = ('name',)
+    pagination_class = DefaultPagination
 
 class FoodItemList(ListAPIView):
     queryset=FoodItem.objects.all()
@@ -17,4 +25,5 @@ class FoodItemList(ListAPIView):
     filter_backends = (DjangoFilterBackend,SearchFilter)
     filter_fields=('price',)
     search_fields=('name', 'description')
+    pagination_class = DefaultPagination
 
