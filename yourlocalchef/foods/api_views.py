73da40572a3d9f5db7d7c6b8747a2dd068f837cd
Marbo Cheng Ye <marbochengye@gmail.com>
2020-07-03 22:42:38 -0700

    API views added filter for User and FoodItem models

--- yourlocalchef/foods/api_views.py
+++ yourlocalchef/foods/api_views.py
@@ -1,13 +1,17 @@
 from rest_framework.generics import ListAPIView
-
+from django_filters.rest_framework import DjangoFilterBackend
 from foods.serializers import UserSerializer,FoodItemSerializer
 from foods.models import User,FoodItem,SaleHistory
 
 class UserList(ListAPIView):
     queryset=User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields=('name','location',)
 
 class FoodItemList(ListAPIView):
     queryset=FoodItem.objects.all()
     serializer_class= FoodItemSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields=('price',)
 
