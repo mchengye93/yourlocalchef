91ee451344f6674ec98413ba22a10ef3f1154dbd
Marbo Cheng Ye <marbochengye@gmail.com>
2020-07-05 14:49:29 -0700

    Added axios library to get API

--- react-express-starter/client/src/components/App.jsx
+++ react-express-starter/client/src/components/App.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import SearchFoodForm from './SearchFoodForm.jsx';
 import FoodItemList from './FoodItemList.jsx';
+import axios from 'axios';
 
 class App extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodItems: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/api/v1/users/")
+    .then(res => {
+      console.log(res);
+      this.setState({
+      foodItems: res.data.results
+      });
+    })
+    .catch(error => {
+      // handle error
+      console.log(error);
+    })
+    
+    
+  }
+
   render() {
     return (
       <div>
         <SearchFoodForm/>
-        <FoodItemList  foodItems={[{name:'Pineapple Bun', description:'Pineaple', price:1.90}]}/>
+        <FoodItemList  foodItems={this.state.foodItems}/>
       </div>
     );
   }
